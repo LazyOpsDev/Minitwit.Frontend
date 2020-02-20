ff8537d88057a2b06c0bf8fb9c231a9e7010ca49
banksdev <alsb@itu.dk>
2020-02-20 16:47:07 +0100

    add test data

---
+++ flutter/minitwit_frontend/lib/test_data/globals.dart
@@ -0,0 +1,14 @@
+import 'package:minitwit_frontend/models/tweet.dart';
+
+final publicTweets = [
+    Tweet(author: "John doe", msg: "Hello I am John Doe"),
+    Tweet(author: "Bob", msg: "I like people who are called Alice"),
+    Tweet(author: "Alice", msg: "How do I establish secure communication?"),
+    Tweet(author: "Mallory", msg: "<script>alert('mwuhahaha')</script>"),
+    Tweet(author: "Oracle", msg: "I am god."),
+    Tweet(author: "123", msg: "I am 123, I am smart"),
+  ];
+
+final privateTweets = [
+    Tweet(author: "123", msg: "I am 123, I am smart"),
+];

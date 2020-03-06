834c1af6ba74cb91fca48c22fe67a16bb170e566
banksdev <alsb@itu.dk>
2020-03-06 20:56:23 +0100

    update backend to new format

--- minitwit_frontend/lib/services/backend.dart
+++ minitwit_frontend/lib/services/backend.dart
@@ -2,8 +2,9 @@ import 'package:http/http.dart' as http;
 import 'package:minitwit_frontend/models/tweet.dart';
 import 'dart:convert' as convert;
 
-// final baseurl = "http://207.154.252.199/";
-final baseurl = "http://localhost/";
+import 'package:minitwit_frontend/models/user.dart';
+
+final baseurl = "http://104.248.141.188/";
 
 final String _logout = "logout";
 final String _login = "login";
@@ -27,4 +28,52 @@ Future<List<Tweet>> getPublicTimeline() async {
   }
 
   return tweetList;
+}
+
+Future<List<Tweet>> getUserTimeline(User user) async {
+  var resp = await http.get("$baseurl${user.username}");
+  if (resp.statusCode != 200) 
+    return null;
+  
+  var tweets = convert.jsonDecode(resp.body);
+  var tweetList = List<Tweet>();
+  for(var t in tweets) {
+    var tweet = Tweet.fromJson(t);
+    tweetList.add(tweet);
+  }
+
+  return tweetList;
+}
+
+
+Future<User> login(String username, String password) async {
+  // var resp = await http.post("$baseurl$_login", 
+  //   body: { "Username": username, "Password": password }
+  // );
+  // if (resp.statusCode != 200) 
+  //   return null;
+  
+  // var user = convert.jsonDecode(resp.body);
+  // return User(username: user.Username);
+
+  return User(username: username, password: password);
+}
+
+Future<User> register(RegisterUser register) async {
+  var resp = await http.post("$baseurl$_register", 
+    body: { 
+      "username": register.username, 
+      "pwd": register.password,  
+      "email": register.email
+      }
+  );
+
+  if (resp.statusCode != 204) {
+    print(resp.statusCode);
+    return null;
+  }
+  
+  var user = User(username: register.username, password: register.password);
+  user.cookie = "mysecretcookie"; // TODO: Fix this somehow?
+  return user;
 }
